a29935b70f1dc86c5f45bc27b334417ae2aa468e
Alexander Kolomoets <0985305543@ukr.net>
2022-11-14 12:22:05 +0200

    Update models.py

--- tasks/models.py
+++ tasks/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=65, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
     class Meta:
         ordering = ["name"]
